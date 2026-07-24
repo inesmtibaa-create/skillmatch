9dfb0ea259d793e9d34dc736a5b703b5fe3d0927
inesmtibaa-create <ines.mtibaa@insat.ucar.tn>
2026-07-24 16:24:48 +0100

    synonymes updated

--- synonymes.py
+++ synonymes.py
@@ -1,6 +1,27 @@
 SYNONYMES = {
+    # Data & IA
+    'ia': 'intelligence artificielle',
+    'ml': 'machine learning',
+    'nlp': 'traitement du langage naturel',
+
+    # Ressources Humaines
+    'rh': 'ressources humaines',
+    'sirh': 'système information ressources humaines',
+
+    # Informatique & Web
     'dev': 'développement',
-    'devops': 'développement opérations',
-    'ai': 'intelligence artificielle',
-    # Ajoute ici au fur et à mesure
+    'bdd': 'base de données',
+    'bd': 'base de données',
+    'ci/cd': 'intégration continue déploiement continu',
+    'api': 'interface programmation applicative',
+
+    # Génie Civil & BTP
+    'btp': 'bâtiment travaux publics',
+
+    # Finance & Comptabilité
+    'ifrs': 'normes comptables internationales',
+
+    # Marketing & Digital
+    'seo': 'référencement naturel',
+    'sea': 'référencement payant',
 }
